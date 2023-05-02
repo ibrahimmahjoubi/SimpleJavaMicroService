edc1b6cf1e7603dc3eaeb30fbb6b492c203f3037
brahim.mahjoubi <brahim.mahjoubi@maibornwolff.de>
2023-05-02 23:39:44 +0100

    First commit.

---
+++ Apps/Service-B/src/main/java/com/ivra/App.java
@@ -0,0 +1,15 @@
+package com.ivra;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Service-B");
+    }
+}
